8ea82fe0e66b46fbdbf430f4730000112094f604
EEA-SYZ <eea_syz@126.com>
2025-04-12 10:11:53 +0800

    实现了帐户与权限的两个接口

--- test/test.cpp
+++ test/test.cpp
@@ -4,16 +4,32 @@
 #include "student_system.hpp"
 #include "transmitter.hpp"
 
+using namespace std;
+using namespace trm;
+
 int main()
 {
     auto &ssys = SSys::Get();
 
-    auto reply = ssys.CheckAccount(trm::Infomation{trm::rqs::CHECK_ACCOUNT, "adm", "123"});
+    auto f = [](const std::vector<Infomation> &series) {
+        for (const auto &info : series) {
+            for (const auto &each : info) {
+                cout << each << endl;
+            }
+            cout << endl;
+        }
+    };
 
-    for (const auto &each : reply) {
-        std::cout << each << std::endl;
-    }
- 
+    f({
+        ssys.CheckAccount({trm::rqs::CHECK_ACCOUNT, "admin", "123456"}), 
+        ssys.CheckAccount({trm::rqs::CHECK_ACCOUNT, "adm", "123"}), 
+        ssys.CreateAccount({trm::rqs::CREATE_ACCOUNT, Account{"6", "6"}}), 
+        ssys.CheckAccount({trm::rqs::CHECK_ACCOUNT, "6", "6"}), 
+        ssys.CheckAccount({trm::rqs::CHECK_ACCOUNT, "6", "666"}), 
+    });
+
+    while (1) ;
+
     return 0;
 }
 
