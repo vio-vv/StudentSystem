32c3da9d33cbfb79cc0d1ba981311ad0354b8d65
cse24lnab <2385235556@qq.com>
2025-05-19 22:57:12 +0800

    Course and reserve (#16)
    * 写了course和reserve的管理员的接口，然后course和reserve原先那几个接口也有改动
    
    * 课程系统的基础功能和测试代码写好了
    
    * reserve的接口管理员部分基础功能也写好了
    
    * 忽略刚才那两个commit，这个才是写好的course
    
    * reserve的接口也好了
    
    * 为了能再push上去删了一个注释
    
    * 其实基本没写，但是发现reserve里面用了旧的Push,为了方便维护先commit和push了再说，然后给部分ui函数加了brief,也是为了自己更可读
    
    * 修改了一个语法错误
    
    * 刚才push的那个我那边收不到，所以在这边重新push一下
    
    * 课程系统的页面写好了
    
    * reserve的界面，大概率会修改很多，然后course的界面加了个按钮，还没加好
    
    * reserve和course的学生页面初步写好，准备开始写逻辑
    
    * 修复了lab.cpp中的一个错误
    
    * 刚才push不上去

--- source/lab.cpp
+++ source/lab.cpp
@@ -502,3 +502,13 @@ void lab::Reserve::Load(ui::Screen *screen) noexcept
         }
     }
 }
+
+void lab::Reserve::Logic(ui::Screen *screen) noexcept
+{
+    ;
+}
+//刚才push不上去，写个注释
+void lab::Reserve::Ready(ui::Screen *screen) noexcept
+{
+    ;
+}
